162de4621c8fa8584fb62993d4608361d5e001a4
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-21 01:23:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,11 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
     SQL
     DB[:conn].execute(sql)
   end
