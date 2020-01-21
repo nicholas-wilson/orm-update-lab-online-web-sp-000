70fd0f7ba639ff5f1967206e12d6851b5a304209
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-21 01:49:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,8 +36,11 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
+      LIMIT 1
     SQL
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.create_table
